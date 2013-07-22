4006e5e388ee2cd4ef58867223977459f9efe22b
Chris Gat <chris.gat@gmail.com>
2013-07-22 20:48:56 +1200

    seeds.rb now seeds project, station, historic image, repeat image, and user repeat image

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,22 @@ puts 'user: ' << user.name
 user.add_role :admin
 user.skip_confirmation!
 user.save!
+
+lorem <<-text
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus condimentum leo non eros viverra, eget accumsan risus tincidunt.
+Morbi gravida fermentum viverra. Maecenas pulvinar convallis commodo.
+Duis tincidunt tellus dolor, ornare interdum nisi euismod ac.
+text
+project = Project.create(name: "Moving Ice", description: lorem)
+stations = Station.create([{name: "Illecillewaet Glacier", description: lorem, project: project},{name: "Athabasca Glacier", description: lorem, project: project}])
+h_image = HistoricImage.create!(station: stations.first, date: Date.new(1887))
+h_image.image.store!(File.open(File.join(Rails.root, "app","assets", "images",'NOT1887_V1707_background.jpg')))
+h_image.save!
+r_image = RepeatImage.create!(historic_image: h_image, date: Date.new(2011))
+r_image.image.store!(File.open(File.join(Rails.root, "app","assets","images","MLP2011_V1707_background.jpg")))
+r_image.save!
+
+test_user = User.find_or_create_by_email name: ENV['TEST_NAME'].dup, email: ENV['TEST_EMAIL'].dup, password: ENV['TEST_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD'].dup
+t_image = UserRepeatImage.create!(user: test_user, date: Date.now)
+t_image.image.store!(File.open(File.join(Rails.root, "app","assets","images","MLP2011_V1707_background.jpg")))
+
