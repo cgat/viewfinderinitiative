068f34763435a373f93c96473b3f69b671ae0e5d
Chris Gat <chris.gat@gmail.com>
2013-07-31 12:09:26 +1200

    removed production secret token from secret_token file. now requires an environment variable (in production)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Viewfinderinitiative::Application.config.secret_token = '4e9d54be9913079fe7120727c4b238804a97af9d8882b3d259617f9c125b21a95bfa36c5912acac5961ce5fc1a9a606e9136c3c01f49826e094a7a10815ba827'
+if Rails.env.test? || Rails.env.development? || Rails.env == "profile"
+  Viewfinderinitiative::Application.config.secret_token = '4e9d54be9913079fe7120727c4b238804a97af9d8882b3d259617f9c125b21a95bfa36c5912acac5961ce5fc1a9a606e9136c3c01f49826e094a7a10815ba827'
+else
+  raise "You must set a secret token in ENV['SECRET_TOKEN'] or in config/initializers/secret_token.rb" if ENV['SECRET_TOKEN'].blank?
+  Viewfinderinitiative::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
